d9658904e0843c1c24b7c96693fb2215fa2009a2
Sudhanshu Jha <Sudhanshujha2717@gmail.com>
2025-06-09 17:46:59 +0530

    updated

--- models/incentive.model.js
+++ models/incentive.model.js
@@ -2,11 +2,11 @@ import { pool } from '../db/pool.js';
 
 export const IncentiveModel = {
   async create(data) {
-    const { userId, sales, availability_rate, clusterId, from_date, to_date } = data;
+    const { userId, sales, availability_rate, clusterId, from_date, to_date, salesPerMachine } = data;
     const res = await pool.query(
-      `INSERT INTO incentives (userId, sales, availability_rate, clusterId, from_date, to_date, createdAt)
-       VALUES ($1, $2, $3, $4, $5, $6, NOW()) RETURNING *`,
-      [userId, sales, availability_rate, clusterId, from_date, to_date]
+      `INSERT INTO incentives (userId, sales, availability_rate, clusterId, from_date, to_date,salesPerMachine, createdAt)
+       VALUES ($1, $2, $3, $4, $5, $6, $7, NOW()) RETURNING *`,
+      [userId, sales, availability_rate, clusterId, from_date, to_date,salesPerMachine]
     );
     return res.rows[0];
   },
@@ -23,11 +23,11 @@ export const IncentiveModel = {
   },
 
   async update(id, data) {
-    const { sales, availability_rate, from_date, to_date } = data;
+    const { sales, availability_rate, from_date, to_date, salesPerMachine} = data;
     const res = await pool.query(
-      `UPDATE incentives SET sales=$1, availability_rate=$2, from_date=$3, to_date=$4
-       WHERE id=$5 RETURNING *`,
-      [sales, availability_rate, from_date, to_date, id]
+      `UPDATE incentives SET sales=$1, availability_rate=$2, from_date=$3, to_date=$4, salesPerMachine=$5
+       WHERE id=$6 RETURNING *`,
+      [sales, availability_rate, from_date, to_date,salesPerMachine, id]
     );
     return res.rows[0];
   },
